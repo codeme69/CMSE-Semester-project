2f5425c1961227aeefa37ee80b59fef2587de547
Sri Ram Seelamneni <seelamne@ganymede.egr.msu.edu>
2023-04-06 17:05:10 -0400

    Defined model class and Added some functions

--- Model.py
+++ Model.py
@@ -1,1 +1,42 @@
+import random 
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+class model():
+    '''
+    A model class
+    Contains the users and all information related
+    '''
+    def __init__(self,x,y,capacity=100):
+        '''
+        Initializes the model object 
+        x (int) :size of model
+        y (int) :size of model
+        capacity (int) : maximum numbers of user allowed in model
+        '''
+        self.x = x
+        self.y = y
+        self.users = []
+        self.capacity = capacity
+        
+    def get_capacity(self):
+        '''
+        Method to get capacity
+        '''
+        return self.capacity
+    
+    def add_user(self,user):
+        '''
+        Method to get capacity
+        '''
+        self.users.append(user)
+        
+    def plot(self):
+        x_coords = [np.random.uniform(self.x-1, self.x+1) for user in self.users]
+        y_coords = [np.random.uniform(self.y-1, self.y+1) for user in self.users]
+        colors = np.random.rand(len(self.users))
+        plt.scatter(x_coords, y_coords, c=colors)
+        plt.show()
 
