97dd3e5938e7e991703e54fec32772e0b36f5869
Sai Pramodh Byrraju <byrrajus@ganymede.egr.msu.edu>
2023-04-19 23:24:48 -0400

    Detection algorithm working

--- Detection.py
+++ Detection.py
@@ -1,58 +1,38 @@
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+df = pd.read_csv('Data.csv')
+#Get shape and head
+df.shape
+df.head()
 
-#
-#
-# def calculate_points(team_data):
-#     points = 0
-#     points += int(team_data[5])
-#     points -= int(team_data[6])
-#     points += 3 * int(team_data[5] > team_data[6])
-#     points += int(team_data[5] == team_data[6])
-#     return points
-#
-#
-# for group, games in group_data.items():
-#
-#
-#     team_data = {}
-# with open("worldcupdata_4.csv", "r") as file:
-#     for line in file:
-#         game = line.strip().split()
-#         if game[0] not in team_data:
-#             team_data[game[0]] = [0, 0, 0, 0, 0]
-#         if game[1] not in team_data:
-#             team_data[game[1]] = [0, 0, 0, 0, 0]
-#         for i in range(4, len(game)):
-#             if game[i].isdigit():
-#                 if i < 8:
-#                     team_data[game[0]][i-4] += int(game[i])
-#                 else:
-#                     team_data[game[2]][i-12] += int(game[i])
-# print(team_data)
-#
-#
-# for game in games:
-#
-#
-#     for i in range(4, 6):
-#         country = game[i]
-#     if country not in team_data:
-#         team_data[country] = [0, 0, 0, 0, 0, 0, 0]
-#
-#
-#         for i in range(4, 12):
-#             team_data[game[i]][i - 4] += int(game[i + 8])
-#
-#
-#     team_points = []
-#     for country, data in team_data.items():
-#         points = calculate_points(data)
-#         team_points.append([country, points, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]])
-#
-#
-#     team_points.sort(key=lambda x: (-x[1], -x[2], -x[3]))
-#
-#
-#     print(group)
-#     for i in range(2):
-#         print(f"{i+1}. {team_points[i][0]} ({team_points[i][1]} points)")
-#     print()
+#DataFlair - Get the labels
+labels=df.label
+labels.head()
+
+
+#DataFlair - Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+#DataFlair - Initialize a TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+#DataFlair - Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+#DataFlair - Initialize a PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+#DataFlair - Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+#DataFlair - Build confusion matrix
+confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
+
+
